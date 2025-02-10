67d932b9bcc6c18c14e3c69fe193b7a97763e29d
limdhun <lmdnhn00@gmail.com>
2025-02-10 17:47:13 +0900

    Add files via upload

---
+++ dday1/src/main/java/org/example/TodayServlet.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.ToString;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class TodayServlet
+ */
+@WebServlet("/today")
+
+public class TodayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TodayServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=UTF-8");
+		java.io.PrintWriter out = response.getWriter();
+		
+		out.write("<div>");
+		out.write("<h1>당신의 오늘 운세는</h1>");
+		out.write("<img src='/imgs/1.jpg'>");//랜덤 숫자
+		out.write("</div>");
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
